f79a75848050f26d9b72d61431cd8db921d5bc6b
Martin Gymer <applemartingymer@gmail.com>
2025-08-02 07:47:33 +0100

    Initial Commit

---
+++ GolfAIApp/GolfAIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfAIAppApp.swift
+//  GolfAIApp
+//
+//  Created by Martin Gymer on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GolfAIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GolfAIAppTests/GolfAIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GolfAIAppTests.swift
+//  GolfAIAppTests
+//
+//  Created by Martin Gymer on 02/08/2025.
+//
+
+import Testing
+@testable import GolfAIApp
+
+struct GolfAIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
